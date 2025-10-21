8ea9b2088b57999efb9fd5e698960f5909e829f9
m9ts <mat3us.gois@gmail.com>
2025-10-21 19:51:47 -0300

    Criando header, derivado, $state

--- intro_svelte/src/App.svelte
+++ intro_svelte/src/App.svelte
@@ -1,47 +1,27 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let nome = "Mateus"
+  let idade = 19
+  
+  import Header from "../src/lib/Header.svelte"
+
+  let contador = $state(0);
+  let contadorSubtrai = $state(50);
+  let mensagem = $derived(contador < 5 ? "Menor que 5" : contador = 0);
+  let color = "blue"
+
+  function adicionarContador(){
+    contador++;
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <Header/>
+  <h2 style = "color: {color}">Contador: {contador}</h2>
+  <h2>{mensagem}</h2>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <button onclick={adicionarContador}>Adicionar</button>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<!-- Interpolação de dados -->
+<!-- <h1>Introdução ao Svelte - {@html nome} que tem {idade} anos</h1>  -->
+
---
+++ intro_svelte/src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<h1>
+    Introdução ao Svelte 5
+</h1>
+
+<style>
+    h1{
+        color: purple;
+        text-align: center;
+    }
+</style>
